3881e3aba0ba96f437a1862025ea1445e3c85f48
Ashwnijagtap24 <jagtapashwini2410@gmail.com>
2022-12-04 01:32:45 +0530

    commi

--- PatternExample/ArrayExample/NumberArrayMax.cs
+++ PatternExample/ArrayExample/NumberArrayMax.cs
@@ -121,4 +121,142 @@ namespace PatternExample.ArrayExample
            
         }
     }
+    // Code for Capital letter
+  class CharExample
+    {
+        public char[] Show(char[] chh)
+        {
+            for(int i=0;i<chh.Length;i++)
+            {
+                if (chh[i] >= 'a' && chh[i] <='z')
+                {
+                    chh[i] = (char)(chh[i] - 32);
+                }
+                else if(chh[i]>='A' && chh[i]<='z')
+                {
+                    chh[i] = (char)(chh[i] + 32);
+                }
+                else
+                {
+                    chh[i] = chh[i];
+
+                }
+            }
+            return chh;
+
+        }
+
+        static void Main(string[] args)
+        {
+            char[] c = { 't', 'a', 'r', 'w', 'y' };
+            CharExample ch = new CharExample();
+            ch.Show(c);
+            Console.WriteLine(string.Join(" ", c));
+            Console.ReadLine();
+
+        }
+    }
+    // write code to creat a char type of array element by its alternative char
+    class CharAlternateelement
+    {
+        public void Display(char[] ch)
+        {
+            for (int i = 0; i < ch.Length; i++)
+            {
+
+                
+                if (ch[i] == 'y' || ch[i] == 'Y' || ch[i] == 'z'||ch[i]=='Z')
+                {
+                    ch[i] = Convert.ToChar(97);
+                    Console.WriteLine(ch);
+                }
+                ch[i] = (char)(ch[i] + 2);
+
+            }
+           // Console.WriteLine(ch);
+        }
+        static void Main(string[] args)
+        {
+            char[] c = { 't', 'a', 'r', 'w', 'y' };
+            CharAlternateelement ca = new CharAlternateelement();
+            ca.Display(c);
+            Console.ReadLine();
+
+
+        }
+    }
+    // replce zero if number is divide by 3
+    class Divisibleby3number
+    {
+        public void Display(int[] arr)
+        {
+            for(int i=0;i<arr.Length;i++)
+            {
+                if (arr[i] % 3 == 0)
+                {
+
+                    Console.WriteLine(0);
+                }
+                else
+                {
+                    Console.WriteLine(arr[i]);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] a = { 3, 5, 4, 6, 9 };
+            Divisibleby3number d = new Divisibleby3number();
+            d.Display(a);
+            Console.ReadLine();
+
+        }
+    }
+    //sum of array
+    class SumOfArray
+    {
+        public void Display(int[] arr)
+        {
+            int sum = 0;
+            for(int i=0;i<arr.Length;i++)
+            {
+                sum = arr[i] + sum;
+            }
+            Console.WriteLine("Sum = " + sum);
+        }
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            SumOfArray s = new SumOfArray();
+            s.Display(a);
+            Console.ReadLine();
+
+        }
+    }
+    //Display the prime number from the Array
+    class PrimeNumberArray
+    {
+        public void Display(int[] arr)
+        {
+            for(int i=2;i<arr.Length;i++)
+            {
+                if(arr[i]%2==0)
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("its prime no  "+arr[i]);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            PrimeNumberArray p = new PrimeNumberArray();
+            p.Display(a);
+            Console.ReadLine();
+        }
+    }
 }
